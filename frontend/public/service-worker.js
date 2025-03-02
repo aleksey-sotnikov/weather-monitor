78923605a3e0e8d145710128f93f953ca8ab205b
Aleksey Sotnikov <aleksey.sotnikov@gmail.com>
2025-03-02 22:33:31 +0300

    pwa

---
+++ frontend/public/service-worker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = "weather-app-cache-v1";
+const urlsToCache = ["/", "/index.html", "/favicon.ico"];
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames
+                    .filter((name) => name !== CACHE_NAME)
+                    .map((name) => caches.delete(name))
+            );
+        })
+    );
+});
