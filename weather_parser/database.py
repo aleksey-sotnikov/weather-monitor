9284e0ccbaeaa424803a1ff2e7fb9e5d1ccdb1eb
Aleksey Sotnikov <aleksey.sotnikov@gmail.com>
2025-03-06 10:03:38 +0300

    bugfix

--- weather_parser/database.py
+++ weather_parser/database.py
@@ -95,7 +95,7 @@ def save_forecast(data):
 
     for row in data:
         cursor.execute("""
-            INSERT OR IGNORE INTO weather_forecast (source, timestamp, temperature, humidity, pressure, wind_speed, wind_gust, wind_dir)
+            INSERT OR IGNORE INTO weather_forecast (source, timestamp, temperature, humidity, pressure, wind_speed, wind_dir, wind_gust)
                 VALUES (?, ?, ?, ?, ?, ?, ?, ?)
         """, (
             row["source"],
